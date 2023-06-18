64022677899f45043c8b1fd62d00f89bb32c816a
Igboanugwo Collins <munisco12@gmail.com>
2023-06-18 17:29:48 +0100

    working on tenants table

--- src/tenants/dto/tenant.dto.ts
+++ src/tenants/dto/tenant.dto.ts
@@ -1,20 +1,31 @@
-
-
-import { IsEmail, IsString, IsNotEmpty } from 'class-validator';
-
-landlord_name: varchar('landlord_name', { length: 100 }).notNull(),
-  landlord_address: varchar('landlord_address', { length: 100 }).notNull(),
-  tenant_name: varchar('tenant_name', { length: 100 }).notNull(),
-  tenant_address: varchar('tenant_address', { length: 100 }).notNull(),
-  property_description: text('property_description'),
-  relevant_documents: text('relevant_documents'),
-  agreement_id: integer('agreement_id').references(() => agreements.id, {
-    onDelete: 'cascade',
-  }),
+import { IsString, IsNotEmpty } from 'class-validator';
 
 export class TenantDto {
   @IsString()
   @IsNotEmpty()
-  lastName: string;
-}
+  landlord_name: string;
 
+  @IsString()
+  @IsNotEmpty()
+  landlord_address: string;
+
+  @IsString()
+  @IsNotEmpty()
+  tenant_name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  tenant_address: string;
+
+  @IsString()
+  @IsNotEmpty()
+  property_description: string;
+
+  @IsString()
+  @IsNotEmpty()
+  relevant_documents: string;
+
+  @IsString()
+  @IsNotEmpty()
+  agreement_id: string;
+}
