3c5912a556b7b8f3ae7c73c47c3d5147cea25b8a
Igboanugwo Collins <munisco12@gmail.com>
2023-06-18 15:40:35 +0100

    working on drizzle

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { DrizzleModule } from 'src/drizzle/drizzle.module';
 
 @Module({
   controllers: [UsersController],
+  imports: [DrizzleModule],
   providers: [UsersService],
 })
 export class UsersModule {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,19 +2,27 @@ import { Inject, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PG_CONNECTION } from 'src/drizzle/constants';
+import { users } from 'src/drizzle/schema';
+import { PostgresJsDatabase } from 'drizzle-orm/postgres-js';
 
 @Injectable()
 export class UsersService {
-  constructor(@Inject(PG_CONNECTION) private conn: any) {}
+  constructor(
+    @Inject(PG_CONNECTION)
+    private conn: PostgresJsDatabase<Record<string, never>>,
+  ) {}
 
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
 
   async findAll() {
-    const users = await this.conn.query.users.findMany();
-    console.log(users);
-    return users;
+    //const users = await this.conn.query.users.findMany();
+
+    const allUsers = await this.conn.select().from(users);
+
+    console.log(allUsers);
+    return allUsers;
   }
 
   findOne(id: number) {
