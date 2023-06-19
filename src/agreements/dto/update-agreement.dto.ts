a82b14b3496cf0a54556760c39dfce207bcafa53
Igboanugwo Collins <munisco12@gmail.com>
2023-06-19 05:53:47 +0100

    removed

--- src/agreements/dto/update-agreement.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateAgreementDto } from './create-agreement.dto';
-
-export class UpdateAgreementDto extends PartialType(CreateAgreementDto) {}
