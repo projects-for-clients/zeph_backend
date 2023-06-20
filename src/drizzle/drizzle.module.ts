c4040857af0055fe83185495e20a308f0614366c
Igboanugwo Collins <munisco12@gmail.com>
2023-06-20 21:55:14 +0100

    working on service

--- src/drizzle/drizzle.module.ts
+++ src/drizzle/drizzle.module.ts
@@ -1,4 +1,4 @@
-import { Logger, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
 import { PG_CONNECTION } from './constants';
