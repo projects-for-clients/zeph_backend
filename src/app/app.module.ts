0f3c740e40da05cf6c873f9a6b9ddc7e23ae6e34
Igboanugwo Collins <munisco12@gmail.com>
2023-06-21 19:31:14 +0100

    working on otp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,33 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from 'src/auth/auth.module';
+import { AgreementsModule } from 'src/models/agreements/agreements.module';
+import { TenantsModule } from 'src/models/tenants/tenants.module';
+import { UsersModule } from 'src/models/users/users.module';
+import { OtpModule } from 'src/otp/otp.module';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
-@Module({})
-export class UserModule {}
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    CacheModule.register({
+      isGlobal: true,
+    }),
+
+    UsersModule,
+    AuthModule,
+    PrismaModule,
+    UsersModule,
+    AgreementsModule,
+    TenantsModule,
+    OtpModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,7 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject, CACHE_MANAGER } from '@nestjs/common';
+import { Cache } from 'cache-manager';
 
 @Injectable()
 export class AppService {
+  constructor(@Inject(CACHE_MANAGER) private readonly cacheManager: Cache) {}
+
   getHello(): string {
     return 'Hello World!';
   }
