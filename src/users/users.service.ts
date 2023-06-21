f6c8a62e10f806f000deb66b8ce84df2212a9805
Igboanugwo Collins <munisco12@gmail.com>
2023-06-21 09:07:18 +0100

    update

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,7 @@ import { PrismaService } from './../prisma/prisma.service';
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { users } from '@prisma/client';
 
 @Injectable()
 export class UsersService {
