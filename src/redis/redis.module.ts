da018620fc90b7f1de65501aca270ea27785e8b3
Igboanugwo Collins <munisco12@gmail.com>
2023-06-22 11:54:53 +0100

    working on redis

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
-import { CacheModule } from '@nestjs/common';
 import * as redisStore from 'cache-manager-redis-yet';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { RedisService } from './redis.service';
-import { CacheInterceptor } from '@nestjs/cache-manager';
+import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
 
 @Module({
   imports: [
