42bca9144fec3e594cf66371a9ec762bf2cd28e0
Igboanugwo Collins <munisco12@gmail.com>
2023-06-22 21:06:44 +0100

    working on middlewares

--- src/pipes/auth.pipe.ts
+++ src/pipes/auth.pipe.ts
@@ -1,8 +1,10 @@
-import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
+import { ArgumentMetadata, Injectable, PipeTransform, Logger } from '@nestjs/common';
 
 @Injectable()
 export class AuthPipe implements PipeTransform {
+  private readonly logger = new Logger(AuthPipe.name);
   transform(value: any, metadata: ArgumentMetadata) {
+    this.logger.log(AuthPipe.name, value, metadata);
     return value;
   }
 }
