1ad35c6f26371946a87c5487147df1c1e8221c37
Igboanugwo Collins <munisco12@gmail.com>
2023-06-22 05:30:16 +0100

    added redis

--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -5,4 +5,21 @@ import { Cache } from 'cache-manager';
 @Injectable()
 export class RedisService {
   constructor(@Inject(CACHE_MANAGER) private readonly cache: Cache) {}
+
+  async setCache(key: string, value: any): Promise<void> {
+    await this.cache.set(key, value, { ttl: 1000 });
+  }
+
+  async getCache(key: string): Promise<unknown> {
+    const cached = await this.cache.get(key);
+    return cached;
+  }
+
+  async delCache(key: string): Promise<void> {
+    await this.cache.del(key);
+  }
+
+  async resetCache(): Promise<void> {
+    await this.cache.reset();
+  }
 }
