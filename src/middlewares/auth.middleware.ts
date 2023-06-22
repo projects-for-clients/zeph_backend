d93feef5835a24263187cfe4b2d39e6b631a7c7c
Igboanugwo Collins <munisco12@gmail.com>
2023-06-22 18:46:55 +0100

    working on middlewares

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,7 +1,7 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 
 @Injectable()
-export class AppMiddleware implements NestMiddleware {
+export class AuthMiddleware implements NestMiddleware {
   use(req: any, res: any, next: () => void) {
     next();
   }
