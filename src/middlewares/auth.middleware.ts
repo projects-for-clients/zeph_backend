202426c1c5eea4e5ba31fb6007c3bd9f38462fd7
Igboanugwo Collins <munisco12@gmail.com>
2023-06-22 18:55:13 +0100

    working on middlewares

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,7 +1,10 @@
+import { RequestService } from './../services/request.service';
 import { Injectable, NestMiddleware } from '@nestjs/common';
 
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
+  constructor(private request: RequestService) {}
+
   use(req: any, res: any, next: () => void) {
     next();
   }
--- src/middlewares/tests/auth.middleware.spec.ts
+++ src/middlewares/tests/auth.middleware.spec.ts
@@ -1,7 +1,12 @@
+import { RequestService } from 'src/services/request.service';
 import { AuthMiddleware } from '../auth.middleware';
 
 describe('AuthMiddleware', () => {
+  const request: RequestService = new RequestService();
   it('should be defined', () => {
-    expect(new AuthMiddleware()).toBeDefined();
+    expect(
+      new AuthMiddleware(request),
+      // new RequestService(),
+    ).toBeDefined();
   });
 });
