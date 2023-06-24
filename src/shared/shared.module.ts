0515e08c57ce2f054051e6da76f29d514874311a
Igboanugwo Collins <munisco12@gmail.com>
2023-06-24 08:35:28 +0100

    further refinement

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,11 +1,6 @@
-import { UsersService } from './../models/users/users.service';
 import { Global, Module } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { AuthController } from 'src/auth/auth.controller';
 import { AuthService } from 'src/auth/auth.service';
-import { TenantsController } from 'src/models/tenants/tenants.controller';
-import { TenantsService } from 'src/models/tenants/tenants.service';
-import { UsersController } from 'src/models/users/users.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { RedisService } from 'src/redis/redis.service';
 import { RequestService } from 'src/services/request.service';
