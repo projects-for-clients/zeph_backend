9d5a3bd50c63b48fe233a131f63ef7da67610bb6
Igboanugwo Collins <munisco12@gmail.com>
2023-06-24 06:10:53 +0100

    working on clean code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,11 @@
-import { RequestService } from './../services/request.service';
 import { MiddlewareConsumer, Module, NestModule, Scope } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AuthModule } from 'src/auth/auth.module';
-import { AgreementsModule } from 'src/models/agreements/agreements.module';
-import { TenantsModule } from 'src/models/tenants/tenants.module';
-import { OtpModule } from 'src/otp/otp.module';
-import { PrismaModule } from 'src/prisma/prisma.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthMiddleware } from 'src/middlewares/auth.middleware';
 import { AuthGuard } from 'src/guards/auth.guard';
 import { LogInterceptor } from 'src/interceptors/app.interceptor';
-import { RedisCacheModule } from 'src/redis/redis.module';
-import { RedisService } from 'src/redis/redis.service';
-import { UsersService } from 'src/models/users/users.service';
-import { UsersController } from 'src/models/users/users.controller';
-import { TenantsService } from 'src/models/tenants/tenants.service';
-import { TenantsController } from 'src/models/tenants/tenants.controller';
+controller';
 import { SharedModule } from 'src/shared/shared.module';
 import { UsersModule } from 'src/models/users/users.module';
 
