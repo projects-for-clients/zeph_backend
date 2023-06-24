a1abde9a1d21eac582f9bae3f3b59f6e4969b97b
Igboanugwo Collins <munisco12@gmail.com>
2023-06-24 06:15:03 +0100

    working on clean code

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -3,15 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello() {
-    return 'Hello World!';
+    return 'Welcome!';
   }
-
-  // async setCache(): Promise<void> {
-  //   await this.cacheManager.set('key', { name: 'John' }, 1000);
-  // }
-
-  // async getCache(): Promise<unknown> {
-  //   const cached = await this.cacheManager.get('key');
-  //   return cached;
-  // }
 }
