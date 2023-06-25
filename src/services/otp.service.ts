46022970b0586d9e5791af23741e891a96ac84aa
Igboanugwo Collins <munisco12@gmail.com>
2023-06-25 12:48:15 +0100

    working on auth middleware

--- src/services/otp.service.ts
+++ src/services/otp.service.ts
@@ -2,19 +2,9 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class OtpService {
-  create(createOtpDto: string) {
-    return 'This action adds a new otp';
-  }
+  
+  constructor() {
 
-  findAll() {
-    return `This action returns all otp`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} otp`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} otp`;
+    
   }
 }
