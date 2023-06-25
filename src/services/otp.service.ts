5c1b94b3f27ac0f408348771751cdf28032a00b5
Igboanugwo Collins <munisco12@gmail.com>
2023-06-25 12:59:02 +0100

    working on auth middleware

--- src/services/otp.service.ts
+++ src/services/otp.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { RedisService } from 'src/redis/redis.service';
 
 @Injectable()
 export class OtpService {
   private otp: number;
 
+  constructor(private redis: RedisService) {}
+
   generateOtp() {
     this.otp = Math.floor(100000 + Math.random() * 900000);
+    
   }
 
   deleteOtp() {
