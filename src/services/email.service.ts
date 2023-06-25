8761b66b02aeba7d78bb10c6a0e7c17f6ee84ff3
Igboanugwo Collins <munisco12@gmail.com>
2023-06-25 07:54:12 +0100

    working on auth middleware

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -5,23 +5,26 @@ import { Resend } from 'resend';
 export class EmailService {
   private resend = new Resend('re_8CJiZ1Dk_J3svncpZvh5mL5vgyfGR7qjg');
 
-  constructor(
-    private to: string,
-    private from: string,
-    private subject: string,
-    private html: string,
-  ) {}
+  //   constructor(
+  //     private to: string,
+  //     private from: string,
+  //     private subject: string,
+  //     private html: string,
+  //   ) {}
 
   async send() {
     try {
-      this.resend.emails.send({
-        from: 'onboarding@zephChambers.support',
+      console.log('sending email=====================');
+      const initSend = await this.resend.emails.send({
+        from: 'onboarding@resend.dev',
         to: 'zephchambersdev@gmail.com',
         subject: 'Hello World',
         html: '<p>Congrats on sending your <strong>first email</strong>!</p>',
       });
+
+      console.log({ initSend });
     } catch (err) {
-      console.log(err);
+      console.log({ err });
     }
   }
 }
