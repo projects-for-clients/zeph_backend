ed677b129cc15cae32832248a295241e760d659f
Igboanugwo Collins <munisco12@gmail.com>
2023-06-27 13:20:41 +0100

    added local file upload

--- src/pipes/parseFormDataJson.pipe.ts
+++ src/pipes/parseFormDataJson.pipe.ts
@@ -1,23 +1,27 @@
-import { PipeTransform, ArgumentMetadata } from '@nestjs/common';
-import { deepParseJson } from 'deep-parse-json';
-import * as _ from 'lodash';
+import { PipeTransform, ArgumentMetadata } from "@nestjs/common";
+import { deepParseJson } from "deep-parse-json";
+import * as _ from "lodash";
 
 type TParseFormDataJsonOptions = {
-  except?: string[];
+	except?: string[];
 };
 
 export class ParseFormDataJsonPipe implements PipeTransform {
-  constructor(private options?: TParseFormDataJsonOptions) {}
+	constructor(private options?: TParseFormDataJsonOptions) {}
 
-  transform(value: any, _metadata: ArgumentMetadata) {
-    const { except } = this.options;
-    const serializedValue = value;
-    const originProperties = {};
-    if (except?.length) {
-      _.merge(originProperties, _.pick(serializedValue, ...except));
-    }
-    const deserializedValue = deepParseJson(value);
+	// rome-ignore lint/suspicious/noExplicitAny: <explanation>
+	transform(value: any, _metadata: ArgumentMetadata) {
+		const { except } = this.options;
+		const serializedValue = value;
+		const originProperties = {};
+		if (except?.length) {
+			_.merge(originProperties, _.pick(serializedValue, ...except));
+		}
 
-    return { ...deserializedValue, ...originProperties };
-  }
+		console.log({ value });
+
+		const deserializedValue = deepParseJson(value);
+
+		return { ...deserializedValue, ...originProperties };
+	}
 }
