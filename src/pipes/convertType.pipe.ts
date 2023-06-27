c0141e83f17fdbbfc32a6019f6ec009d8170aeeb
Igboanugwo Collins <munisco12@gmail.com>
2023-06-27 12:03:43 +0100

    added local file upload

--- src/pipes/convertType.pipe.ts
+++ src/pipes/convertType.pipe.ts
@@ -1,43 +1,50 @@
 import {
-  ArgumentMetadata,
-  Injectable,
-  PipeTransform,
-  BadRequestException,
-} from '@nestjs/common';
+	ArgumentMetadata,
+	Injectable,
+	PipeTransform,
+	BadRequestException,
+} from "@nestjs/common";
 
 @Injectable()
 export class ConvertTypePipe implements PipeTransform {
-  constructor(private readonly options: Record<'key' | 'toType', string>[]) {}
+	constructor(private readonly options: Record<"key" | "toType", string>[]) {}
 
-  transform(value: any, metadata: ArgumentMetadata) {
-    const errors: string[] = [];
-    const convertedTypes = this.options.map((option) => {
-      const { key, toType } = option;
-      if (metadata.type === 'body') {
-        if (key in value) {
-          const numberCheck = toType === 'number' && Number(value[key]);
+	// rome-ignore lint/suspicious/noExplicitAny: <explanation>
+	transform(value: any, metadata: ArgumentMetadata) {
+		const errors: string[] = [];
+		const convertedTypes = this.options.map((option) => {
+			const { key, toType } = option;
+			if (metadata.type === "body") {
+				if (key in value) {
+					const numberCheck = toType === "number" && Number(value[key]);
 
-          if (!numberCheck) {
-            return errors.push(`${key} is not a ${toType}`);
-          }
+					if (!numberCheck) {
+						return errors.push(`${key} is not a ${toType}`);
+					}
 
-          return { [key]: numberCheck };
-        }
-      }
-    });
+					return { [key]: numberCheck };
+				}
+			}
+		});
 
-    const updatedValue = {
-      ...value,
-      ...convertedTypes.reduce(
-        (acc: any, curr: any) => ({ ...acc, ...curr }),
-        {},
-      ),
-    };
+		const updatedValue = {
+			...value,
+			...convertedTypes.reduce(
+				// rome-ignore lint/suspicious/noExplicitAny: <explanation>
+				(acc: any, curr: any) => ({
+					...acc,
+					...curr,
+				}),
+				{},
+			),
+		};
 
-    if (errors.length) {
-      throw new BadRequestException(errors);
-    }
+		console.log({ updatedValue, errors });
 
-    return { ...updatedValue };
-  }
+		if (errors.length) {
+			throw new BadRequestException(errors);
+		}
+
+		return { ...updatedValue };
+	}
 }
