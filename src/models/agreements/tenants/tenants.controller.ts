0238151674efb5461e50954f9363df977e243ba5
Igboanugwo Collins <munisco12@gmail.com>
2023-06-27 13:11:04 +0100

    added local file upload

--- src/models/agreements/tenants/tenants.controller.ts
+++ src/models/agreements/tenants/tenants.controller.ts
@@ -1,70 +1,74 @@
 import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-  Headers,
-  UseInterceptors,
-  ValidationPipe,
-  UploadedFile,
-  UsePipes,
-} from '@nestjs/common';
-import { ApiConsumes } from '@nestjs/swagger';
-import { TenantsService } from './tenants.service';
-import { TenantDto } from './dto';
-import { ParseFormDataJsonPipe } from 'src/pipes/parseFormDataJson.pipe';
-import { FileInterceptor } from '@nestjs/platform-express';
-import { imgConfig } from 'src/config/img.config';
-import { ConvertTypePipe } from 'src/pipes/convertType.pipe';
+	Controller,
+	Get,
+	Post,
+	Body,
+	Patch,
+	Param,
+	Delete,
+	Headers,
+	UseInterceptors,
+	ValidationPipe,
+	UploadedFile,
+	UsePipes,
+	UploadedFiles,
+} from "@nestjs/common";
+import { ApiConsumes } from "@nestjs/swagger";
+import { TenantsService } from "./tenants.service";
+import { TenantDto } from "./dto";
+import { ParseFormDataJsonPipe } from "src/pipes/parseFormDataJson.pipe";
+import { FileInterceptor } from "@nestjs/platform-express";
+import { imgConfig } from "src/config/img.config";
+import { ConvertTypePipe } from "src/pipes/convertType.pipe";
 
 @UsePipes(
-  new ConvertTypePipe([
-    {
-      key: 'amount',
-      toType: 'number',
-    },
-  ]),
+	new ConvertTypePipe([
+		{
+			key: "amount",
+			toType: "number",
+		},
+	]),
 )
-@Controller('tenants')
+@Controller("tenants")
 export class TenantsController {
-  constructor(private readonly tenantsService: TenantsService) {}
+	constructor(private readonly tenantsService: TenantsService) {}
 
-  @Post()
-  @ApiConsumes('multipart/form-data')
-  @UseInterceptors(
-    FileInterceptor('relevant_documents', {
-      limits: { fileSize: imgConfig.maxFileSize },
-    }),
-  )
-  async create(
-    @Body(new ParseFormDataJsonPipe({ except: ['relevant_documents'] }))
-    tenantDto: TenantDto,
-    @UploadedFile() file: Express.Multer.File,
-  ) {
-    // console.log(`createDto`, tenantDto, `image`, file);
-    return this.tenantsService.create(tenantDto, file);
-  }
+	@Post()
+	@ApiConsumes("multipart/form-data")
+	@UseInterceptors(
+		FileInterceptor("relevant_documents", {
+			limits: { fileSize: imgConfig.maxFileSize },
+		}),
+	)
+	create(
+		// @Body(new ParseFormDataJsonPipe({ except: ['relevant_documents'] }))
+		@Body()
+		tenantDto,
+		@UploadedFiles() files: Array<Express.Multer.File>,
+	) {
+		console.log("created", tenantDto, "file", files);
+		// return this.tenantsService.create(tenantDto, file);
 
-  @Get()
-  findAll() {
-    return this.tenantsService.findAll();
-  }
+		return "hello wolrd";
+	}
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.tenantsService.findOne(+id);
-  }
+	@Get()
+	findAll() {
+		return this.tenantsService.findAll();
+	}
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateTenantDto: TenantDto) {
-    return this.tenantsService.update(+id, updateTenantDto);
-  }
+	@Get(":id")
+	findOne(@Param('id') id: string) {
+		return this.tenantsService.findOne(+id);
+	}
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.tenantsService.remove(+id);
-  }
+	@Patch(":id")
+	update(@Param('id') id: string, @Body() updateTenantDto: TenantDto) {
+		return this.tenantsService.update(+id, updateTenantDto);
+	}
+
+	@Delete(":id")
+	remove(@Param('id') id: string) {
+		return this.tenantsService.remove(+id);
+	}
 }
