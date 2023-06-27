50820906047bfebf49c3a01bf5622fc84def3d96
Igboanugwo Collins <munisco12@gmail.com>
2023-06-27 05:18:45 +0100

    added tenants

--- src/pipes/convertType.pipe.ts
+++ src/pipes/convertType.pipe.ts
@@ -3,24 +3,39 @@ import {
   Injectable,
   PipeTransform,
   Logger,
+  BadRequestException,
 } from '@nestjs/common';
 
 @Injectable()
 export class ConvertTypePipe implements PipeTransform {
   constructor(private readonly options: Record<'key' | 'type', string>[]) {}
   transform(value: any, metadata: ArgumentMetadata) {
+    const errors: string[] = [];
     const covertedTypes = this.options.map((option) => {
       const { key, type } = option;
       if (metadata.type === 'body') {
         if (key in value) {
-          return { [key]: type === 'number' ? Number(value[key]) : value[key] };
+          const numberCheck = type === 'number' && Number(value[key]);
+
+          if (!numberCheck) {
+            console.log({ numberCheck });
+            return errors.push(`${key} is not a ${type}`);
+          }
+          return { [key]: numberCheck ?? value[key] };
         }
       }
       return {};
     });
 
-    console.log({ covertedTypes })
+    console.log({ value, covertedTypes });
 
     console.log({ value, metadata });
+
+    if (errors.length) {
+      // throw new Error(errors.join(', '));
+      throw new BadRequestException(errors);
+    }
+
+    return { ...value, ...covertedTypes };
   }
 }
