42bb6ce0d2bf6fa992bd2dd2697f170baef3fb63
Igboanugwo Collins <munisco12@gmail.com>
2023-06-28 05:26:12 +0100

    added local file upload

--- src/services/uploadFiles.service.ts
+++ src/services/uploadFiles.service.ts
@@ -7,16 +7,17 @@ import { GoogleAuth } from "google-auth-library";
 export class UploadedFilesService {
 	async uploadBasic() {
 		const auth = new GoogleAuth({
-			scopes: "https://www.googleapis.com/auth/drive",
+            scopes: "https://www.googleapis.com/auth/drive",
+            keyFile: "src/credentials/googleKey.json",
 		});
 		const service = google.drive({ version: "v3", auth });
 		const requestBody = {
-			name: "photo.jpg",
+			name: "3000_followers.jpg",
 			fields: "id",
 		};
 		const media = {
-			mimeType: "image/jpeg",
-			body: fs.createReadStream("files/photo.jpg"),
+			mimeType: "image/png",
+			body: fs.createReadStream("uploads/TenantsService/3000_followers.png"),
 		};
 		try {
 			const file = await service.files.create({
