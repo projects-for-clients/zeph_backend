706ae451c718ed319b6f6c6deba342c4a21bff43
Igboanugwo Collins <munisco12@gmail.com>
2023-06-28 14:14:48 +0100

    working on cloudinary

--- src/pipes/fileSize.pipe.ts
+++ src/pipes/fileSize.pipe.ts
@@ -6,23 +6,23 @@ export class FileSizeValidationPipe implements PipeTransform {
         // "value" is an object containing the file's attributes and metadata
 
         console.log({ value, metadata })
+        let mbTotalFileSize = 0
 
         const limitFileSize = 1000 * 1000 * 5; // 5MB
+        
         const handleFileSize = (file: Buffer) => {
-            console.log(file.byteLength)
             const kbSize = Math.floor(file.byteLength / 1000);
 
-            console.log({ kbSize })
-
             const mbSize = Math.floor(kbSize / 1000);
+            mbTotalFileSize += mbSize;
 
-            console.log({ mbSize, limitFileSize })
-
-            if (mbSize > limitFileSize) {
-                throw new Error(`File size too large. Max file size is ${limitFileSize} bytes.`);
-            }
         }
         Object.values(value).forEach((file: Express.Multer.File) => handleFileSize(file.buffer))
+
+
+        if (mbTotalFileSize > limitFileSize) {
+            throw new Error(`File size too large. Max file size is ${limitFileSize} bytes.`);
+        }
 
 
         const oneKb = 1000;
