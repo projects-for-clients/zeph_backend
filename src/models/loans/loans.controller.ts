d30496c3e961f7db80d8f932df6f702daad826ea
Igboanugwo Collins <munisco12@gmail.com>
2023-06-28 20:29:56 +0100

    working on loans

--- src/models/loans/loans.controller.ts
+++ src/models/loans/loans.controller.ts
@@ -1,7 +1,41 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { LoansService } from './loans.service';
+import { createDto, updateDto } from './dto';
 
 @Controller('loans')
 export class LoansController {
-  constructor(private readonly loansService: LoansService) {}
+  constructor(private readonly loansService: LoansService) { }
+
+  @Post()
+  create(@Body() createLeaseDto: createDto) {
+    return this.loansService.create(createLeaseDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.loansService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number) {
+    return this.loansService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: number, @Body() updateLeaseDto: updateDto) {
+    return this.loansService.update(+id, updateLeaseDto);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: number) {
+    return this.loansService.delete(+id);
+  }
 }
