7978ab29f2fdd988817d2815a64c1b5c171410af
Igboanugwo Collins <munisco12@gmail.com>
2023-06-28 14:15:42 +0100

    working on cloudinary

--- src/pipes/fileSize.pipe.ts
+++ src/pipes/fileSize.pipe.ts
@@ -5,7 +5,6 @@ export class FileSizeValidationPipe implements PipeTransform {
     transform(value: any, metadata: any) {
         // "value" is an object containing the file's attributes and metadata
 
-        console.log({ value, metadata })
         let mbTotalFileSize = 0
 
         const limitFileSize = 5; // 5MB
