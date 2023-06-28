e071ae202aee8ddd1d89233bffa1b54c5018c608
Igboanugwo Collins <munisco12@gmail.com>
2023-06-28 13:34:32 +0100

    working on cloudinary

--- src/models/agreements/tenants/tenants.controller.ts
+++ src/models/agreements/tenants/tenants.controller.ts
@@ -38,9 +38,7 @@ export class TenantsController {
 	@UseInterceptors(FilesInterceptor("relevant_documents"))
 	create(
 		tenantDto: TenantDto,
-		@UploadedFiles(new ParseFilePipe(
-			
-		)) files: Array<Express.Multer.File>,
+		@UploadedFiles(new ParseFilePipe()) files: Array<Express.Multer.File>,
 	) {
 		return this.tenantsService.create(tenantDto, files);
 	}
