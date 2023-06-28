db35ff38166bb442de772780a0b19fae27020c7e
Igboanugwo Collins <munisco12@gmail.com>
2023-06-28 16:16:23 +0100

    working on cloudinary

--- src/models/agreements/tenants/tenants.service.ts
+++ src/models/agreements/tenants/tenants.service.ts
@@ -60,11 +60,11 @@ export class TenantsService {
 
 		console.log({ uploadedFiles })
 
-		// const executed = await Promise.all(uploadedFiles.map(async (upload) => {
-		// 	return await upload();
-		// }));
+		const executed = await Promise.all(uploadedFiles.map(async (upload) => {
+			return await upload()();
+		}));
 
-		// console.log({ executed })
+		console.log({ executed })
 
 		const isError = await storeFileHandler(folderPath);
 
