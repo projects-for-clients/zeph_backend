072aa70726dbd0c919c5c86fc9a321ea9ffb7bb1
Niscolinx <munisco12@gmail.com>
2023-08-09 10:50:41 +0100

    removed redis in some places

--- src/models/users/users.service.ts
+++ src/models/users/users.service.ts
@@ -3,18 +3,17 @@ import { PrismaService } from '../../prisma/prisma.service';
 import { Logger, Injectable, Scope } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { RedisService } from 'src/redis/redis.service';
+
 
 @Injectable({ scope: Scope.REQUEST })
 export class UsersService {
   private readonly logger = new Logger(UsersService.name);
 
-  constructor(private redis: RedisService, private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) { }
 
   async findAll() {
     const allUsers = await this.prisma.users.findMany();
-    await this.redis.set(UsersService.name, allUsers);
-
+    
     return allUsers;
   }
 
