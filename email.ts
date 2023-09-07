5eb0388cb03d49526aad536127c8b0ce5ee1a61e
Niscolinx <munisco12@gmail.com>
2023-09-07 11:55:15 +0100

    working on emails

---
+++ email.ts
@@ -0,0 +1,115 @@
+export const welcome = `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<html lang="en">
+
+  <head data-id="__react-email-head"></head>
+  <div id="__react-email-preview" style="display:none;overflow:hidden;line-height:1px;opacity:0;max-height:0;max-width:0">Admin&#x27;s Name, you&#x27;re welcome to SESA<div> ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿</div>
+  </div>
+
+  <body data-id="__react-email-body" style="background-color:rgb(229,239,255);margin-top:auto;margin-bottom:auto;margin-left:auto;margin-right:auto;font-family:ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Arial, Noto Sans, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji">
+    <table align="center" width="100%" data-id="__react-email-container" role="presentation" cellSpacing="0" cellPadding="0" border="0" style="max-width:37.5em;margin-left:auto;margin-right:auto;padding:20px;width:100%">
+      <tbody>
+        <tr style="width:100%">
+          <td>
+            <table align="center" width="100%" data-id="react-email-section" border="0" cellPadding="0" cellSpacing="0" role="presentation" style="margin-top:32px">
+              <tbody>
+                <tr>
+                  <td><img data-id="react-email-img" alt="logo" src="https://sesa-emails.vercel.app/images/dark-logo.png" width="100" height="30" style="display:block;outline:none;border:none;text-decoration:none;margin-top:0px;margin-bottom:0px;margin-left:auto;margin-right:auto" /></td>
+                </tr>
+              </tbody>
+            </table>
+            <table align="center" width="100%" data-id="react-email-section" border="0" cellPadding="0" cellSpacing="0" role="presentation" style="margin-top:32px;background-color:rgb(255,255,255);padding:20px;border-radius:0.5rem;box-shadow:0 0 #0000, 0 0 #0000, 0 1px 3px 0 rgb(0,0,0,0.1), 0 1px 2px -1px rgb(0,0,0,0.1)">
+              <tbody>
+                <tr>
+                  <td><img data-id="react-email-img" alt="welcome" src="https://sesa-emails.vercel.app/images/welcome.png" style="display:block;outline:none;border:none;text-decoration:none;margin-top:0px;margin-bottom:0px;margin-left:auto;margin-right:auto" />
+                    <table align="center" width="100%" data-id="react-email-section" border="0" cellPadding="0" cellSpacing="0" role="presentation" style="display:grid;justify-content:center;margin-top:2rem;margin-bottom:2rem">
+                      <tbody>
+                        <tr>
+                          <td>
+                            <h1 data-id="react-email-heading" style="color:rgb(0,0,0);font-size:24px;text-align:center;padding:0px;margin-left:0px;margin-right:0px;font-weight:600;margin-top:0px;margin-bottom:0px">Welcome to SESA Admin Portal</h1>
+                            <p data-id="react-email-text" style="font-size:12px;line-height:24px;margin:16px 0;color:rgb(0,0,0);text-align:center;margin-top:0px;margin-bottom:0px">You have been invited to join SESA as an administrator.</p>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                    <table align="center" width="100%" data-id="react-email-section" border="0" cellPadding="0" cellSpacing="0" role="presentation" style="display:grid;justify-content:center;margin-top:1rem;margin-bottom:1rem">
+                      <tbody>
+                        <tr>
+                          <td>
+                            <p data-id="react-email-text" style="font-size:14px;line-height:24px;margin:16px 0;color:rgb(0,0,0);margin-top:0px;margin-bottom:0px">Dear Admin&#x27;s Name,</p>
+                            <p data-id="react-email-text" style="font-size:14px;line-height:24px;margin:16px 0;color:rgb(0,0,0);margin-top:0.25rem;margin-bottom:0.25rem">We are thrilled to welcome you to the SESA Admin Management portal! As a valued member of our team, you now have access to powerful tools to streamline your tasks and elevate your estate management experience.</p>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                    <table align="center" width="100%" data-id="react-email-section" border="0" cellPadding="0" cellSpacing="0" role="presentation">
+                      <tbody>
+                        <tr>
+                          <td>
+                            <p data-id="react-email-text" style="font-size:14px;line-height:24px;margin:16px 0;color:rgb(0,0,0)">Your Login Credentials</p>
+                            <table align="center" width="100%" data-id="react-email-row" role="presentation" cellSpacing="0" cellPadding="0" border="0" style="display:flex;justify-content:space-between">
+                              <tbody style="width:100%">
+                                <tr style="width:100%">
+                                  <td data-id="__react-email-column"><img data-id="react-email-img" alt="checkBox" src="https://sesa-emails.vercel.app/images/checkBoxFill.png" style="display:block;outline:none;border:none;text-decoration:none;margin-top:0px;margin-bottom:0px;margin-left:0px;margin-right:0px" /></td>
+                                  <td data-id="__react-email-column">
+                                    <p data-id="react-email-text" style="font-size:14px;line-height:24px;margin:16px 0;color:rgb(0,0,0);margin-top:0px;margin-bottom:0px;margin-left:5px"><span style="font-size:12px;font-weight:500">Username: </span><span>Admin&#x27;s Email Address</span></p>
+                                  </td>
+                                </tr>
+                              </tbody>
+                            </table>
+                            <table align="center" width="100%" data-id="react-email-row" role="presentation" cellSpacing="0" cellPadding="0" border="0" style="display:flex;justify-content:space-between">
+                              <tbody style="width:100%">
+                                <tr style="width:100%">
+                                  <td data-id="__react-email-column"><img data-id="react-email-img" alt="checkBox" src="https://sesa-emails.vercel.app/images/checkBoxFill.png" style="display:block;outline:none;border:none;text-decoration:none;margin-top:0px;margin-bottom:0px;margin-left:0px;margin-right:0px" /></td>
+                                  <td data-id="__react-email-column">
+                                    <p data-id="react-email-text" style="font-size:14px;line-height:24px;margin:16px 0;color:rgb(0,0,0);margin-top:0px;margin-bottom:0px;margin-left:5px"><span style="font-size:12px;font-weight:500">Temporary Password: </span><span>Generated Password</span></p>
+                                  </td>
+                                </tr>
+                              </tbody>
+                            </table>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                    <p data-id="react-email-text" style="font-size:14px;line-height:24px;margin:16px 0;color:rgb(0,0,0)">Please use the button below to accept your invitation and get started.</p><a href="https://sesa.com" data-id="react-email-button" target="_blank" style="box-shadow:1.2px 1.2px 1px 0px #7AAAFF inset, -1.2px -1.2px 1px 0px rgba(122, 170, 255, 0.60) inset;line-height:100%;text-decoration:none;display:flex;max-width:100%;padding:12px 20px;background-color:rgb(6,96,254);border-radius:0.25rem;color:rgb(255,255,255);font-size:12px;font-weight:600;text-decoration-line:none;text-align:center;justify-content:center"><span></span><span style="max-width:100%;display:inline-block;line-height:120%;mso-padding-alt:0px;mso-text-raise:9px">Accept Invitation &amp; Get Started</span><span></span></a>
+                    <p data-id="react-email-text" style="font-size:14px;line-height:24px;margin:16px 0;color:rgb(0,0,0)">For security reasons, you will be required to change your password upon your first login.</p>
+                    <p data-id="react-email-text" style="font-size:14px;line-height:24px;margin:16px 0;color:rgb(0,0,0)">Best regards,<br />The SESA Team.</p>
+                    <table align="center" width="100%" data-id="react-email-section" border="0" cellPadding="0" cellSpacing="0" role="presentation" style="margin-left:0px;margin-right:0px;display:flex;justify-content:center;justify-items:center;text-align:center">
+                      <tbody>
+                        <tr>
+                          <td>
+                            <h1 data-id="react-email-heading" style="color:rgb(0,0,0);font-size:16px;text-align:center;padding:0px;font-weight:600;margin-top:0px;margin-bottom:0px">Have any questions?</h1><a href="https://sesa.com" data-id="react-email-link" target="_blank" style="color:rgb(37,99,235);text-decoration:none;text-decoration-line:underline;margin-top:0.25rem;margin-bottom:0.25rem;font-size:12px">support@sesa.com</a>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            <table align="center" width="100%" data-id="react-email-section" border="0" cellPadding="0" cellSpacing="0" role="presentation" style="margin-top:32px;display:flex;justify-content:center">
+              <tbody>
+                <tr>
+                  <td>
+                    <table align="center" width="100%" data-id="react-email-row" role="presentation" cellSpacing="0" cellPadding="0" border="0" style="display:flex;width:max-content;margin-left:auto;margin-right:auto">
+                      <tbody style="width:100%">
+                        <tr style="width:100%">
+                          <td data-id="__react-email-column" style="padding-left:3px;padding-right:3px"><a href="https://sesa.com" data-id="react-email-link" target="_blank" style="color:#067df7;text-decoration:none"><img data-id="react-email-img" alt="facebook" src="https://sesa-emails.vercel.app/images/facebook.png" width="34" height="34" style="display:block;outline:none;border:none;text-decoration:none;margin-top:0px;margin-bottom:0px;margin-left:auto;margin-right:auto" /></a></td>
+                          <td data-id="__react-email-column" style="padding-left:3px;padding-right:3px"><a href="https://sesa.com" data-id="react-email-link" target="_blank" style="color:#067df7;text-decoration:none"><img data-id="react-email-img" alt="instagram" src="https://sesa-emails.vercel.app/images/instagram.png" width="34" height="34" style="display:block;outline:none;border:none;text-decoration:none;margin-top:0px;margin-bottom:0px;margin-left:auto;margin-right:auto" /></a></td>
+                          <td data-id="__react-email-column" style="padding-left:3px;padding-right:3px"><a href="https://sesa.com" data-id="react-email-link" target="_blank" style="color:#067df7;text-decoration:none"><img data-id="react-email-img" alt="twitter" src="https://sesa-emails.vercel.app/images/twitter.png" width="34" height="34" style="display:block;outline:none;border:none;text-decoration:none;margin-top:0px;margin-bottom:0px;margin-left:auto;margin-right:auto" /></a></td>
+                          <td data-id="__react-email-column" style="padding-left:3px;padding-right:3px"><a href="https://sesa.com" data-id="react-email-link" target="_blank" style="color:#067df7;text-decoration:none"><img data-id="react-email-img" alt="linkedin" src="https://sesa-emails.vercel.app/images/linkedin.png" width="34" height="34" style="display:block;outline:none;border:none;text-decoration:none;margin-top:0px;margin-bottom:0px;margin-left:auto;margin-right:auto" /></a></td>
+                        </tr>
+                      </tbody>
+                    </table>
+                    <p data-id="react-email-text" style="font-size:12px;line-height:24px;margin:16px 0;color:rgb(0,0,0);text-align:center">This email was sent to you because you are involved with SESA&#x27;s services. If you believe you received this email in error or have any concerns, please don&#x27;t hesitate to contact us at <a href="https://sesa.com" data-id="react-email-link" target="_blank" style="color:#067df7;text-decoration:none;text-decoration-line:underline">m-support@sesa.com</a></p>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </body>
+
+</html>`
